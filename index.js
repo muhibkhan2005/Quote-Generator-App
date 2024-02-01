7e6db9ed9dc87c0ddc51c0f2a4eb872aa49aa409
muhibkhan2005 <150272985+muhibkhan2005@users.noreply.github.com>
2024-02-01 09:59:37 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+const api_url = "https://api.quotable.io/random";
+const quote = document.getElementById("quote");
+const author = document.getElementById("author");
+async function getquote(url){
+    const respone = await fetch(url);
+    var data = await respone.json();
+    
+    quote.innerHTML = data.content;
+    author.innerHTML = data.author;
+}
+getquote(api_url);
+function tweet(){
+    window.open("https://twitter.com/intent/tweet?text=" + quote.innerHTML + "---- by " + author.innerHTML, "Tweer Window", "widht=600, height=300");
+}
